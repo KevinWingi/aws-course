d920375046567336b608cb69280061d257835d2e
KevinWingi <kevinwing8@gmail.com>
2019-05-01 20:49:45 +0200

    Revert "Create machine resource"
    This reverts commit 541286ec2b9d74a4c45ffe360b13f6ef7977d505.

--- src/main/java/com/awscourse/service/MachineService.java
+++ src/main/java/com/awscourse/service/MachineService.java
@@ -12,48 +12,33 @@ import com.awscourse.entity.Machine;
 public class MachineService {
 
 	// get machine information
-	public Machine getMachineInformation() {
-
-		try {
-			InetAddress address = InetAddress.getLocalHost();
-			String ip = address.getHostAddress();
-			String name = address.getHostName();
-			String os = System.getProperty("os.name");
-			String arch = System.getProperty("os.arch");
-			String osVersion = System.getProperty("os.version");
-
-			Machine machine = new Machine(ip, name, os, arch, osVersion);
-
-			return machine;
-		} catch (UnknownHostException e) {
-			// TODO: handle exception
-			e.printStackTrace();
-			return null;
-		}
+	public Machine getMachineInformation() throws UnknownHostException {
+		InetAddress address = InetAddress.getLocalHost();
+		String ip = address.getHostAddress();
+		String name = address.getHostName();
+		String os = System.getProperty("os.name");
+		String arch = System.getProperty("os.arch");
+		String osVersion = System.getProperty("os.version");
+		
+		Machine machine = new Machine(ip, name, os, arch, osVersion);
+		
+		return machine;
 	}
-
+	
 	// get detailed machine information
-	public DetailedMachine getDetailedMachineInformation() {
-
-		try {
-			InetAddress address = InetAddress.getLocalHost();
-			String ip = address.getHostAddress();
-			String name = address.getHostName();
-			String os = System.getProperty("os.name");
-			String arch = System.getProperty("os.arch");
-			String osVersion = System.getProperty("os.version");
-			String javaVersion = System.getProperty("java.version");
-			String javaVendor = System.getProperty("java.vendor");
-			String javaVendorUrl = System.getProperty("java.vendor.url");
-
-			DetailedMachine detailedMachine = new DetailedMachine(ip, name, os, arch, osVersion, javaVersion,
-					javaVendor, javaVendorUrl);
-
-			return detailedMachine;
-		} catch (UnknownHostException e) {
-			// TODO: handle exception
-			e.printStackTrace();
-			return null;
-		}
+	public DetailedMachine getDetailedMachineInformation() throws UnknownHostException {
+		InetAddress address = InetAddress.getLocalHost();
+		String ip = address.getHostAddress();
+		String name = address.getHostName();
+		String os = System.getProperty("os.name");
+		String arch = System.getProperty("os.arch");
+		String osVersion = System.getProperty("os.version");
+		String javaVersion = System.getProperty("java.version");
+		String javaVendor = System.getProperty("java.vendor");
+		String javaVendorUrl = System.getProperty("java.vendor.url");
+		
+		DetailedMachine detailedMachine = new DetailedMachine(ip, name, os, arch, osVersion, javaVersion, javaVendor, javaVendorUrl);
+		
+		return detailedMachine;
 	}
 }
